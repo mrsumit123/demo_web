33059cf051d5e9528fa98cd48984497e3c00f572
mrsumit123 <154435701+mrsumit123@users.noreply.github.com>
2025-08-21 00:30:50 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+import streamlit
+
+#streamlit.title("Welcome ro Hacker_City ")
+#streamlit.header("python")
+#streamlit.subheader("java")
+#streamlit.code("""#for i in range(1,5,1):
+    #print("your range is :",i)   """)"""
+
+name = streamlit.text_input("Enter your name : ")
+fname = streamlit.text_input("Enter your father name : ")
+addr = streamlit.text_area("Enter your dil ki baat : ")
+classdata = streamlit.selectbox("Enter your class : ",(1,2,3,4,5,6,7,8,9,10,11,12))
+
+button = streamlit.button("Done")
+if button:
+    streamlit.markdown(f"""
+    Name : {name}
+    Father name : {fname}
+    Address : {addr} 
+    class : {classdata} """)
+
+
+
+
